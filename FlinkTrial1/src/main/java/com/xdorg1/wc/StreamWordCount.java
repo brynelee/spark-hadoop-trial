011b30ab51b8d05aa2e95d71e87d054be560a2d8
Xiaodong Li <bryne_lxd@sina.com>
2022-04-30 16:49:36 +0800

    updated StreamWordCount.java by using arguments "--host spark-master --port 7777" to start the program.

--- FlinkTrial1/src/main/java/com/xdorg1/wc/StreamWordCount.java
+++ FlinkTrial1/src/main/java/com/xdorg1/wc/StreamWordCount.java
@@ -3,11 +3,14 @@ package com.xdorg1.wc;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
+
+import java.lang.reflect.Parameter;
 
 public class StreamWordCount {
 
@@ -15,8 +18,14 @@ public class StreamWordCount {
         // 1. 创建流式执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        // 从参数中提取主机名和端口号
+        // 需要在Idea running configuration中设置“Program Arguments”: "--host spark-master --port 7777"
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String hostname = parameterTool.get("host");
+        Integer port = parameterTool.getInt("port");
+
         // 2. 读取文本流
-        DataStreamSource<String> lineDataStream = env.socketTextStream("spark-master", 7777);
+        DataStreamSource<String> lineDataStream = env.socketTextStream(hostname, port);
 
         // 3. 转换计算
         SingleOutputStreamOperator<Tuple2<String, Long>> wordAndOneTuple = lineDataStream.flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
